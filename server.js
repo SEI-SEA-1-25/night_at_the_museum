9148831f5d4d6457efda019f3b2070d7e7d4111b
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-24 20:46:32 -0800

    Begin toutes and view

---
+++ server.js
@@ -0,0 +1,38 @@
+// required packages
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const rowdy = require("rowdy-logger");
+const axios = require("axios")
+// configure environmental variables
+require("dotenv").config();
+
+const MUSEUM_API_KEY = process.env.MUSEUM_API_KEY;
+
+// confifgure express app
+const app = express();
+const PORT = 3000;
+const rowdyResults = rowdy.begin(app);
+
+// Sets EJS as the view engine
+app.set("view engine", "ejs");
+// Specifies the location of the static assets folder
+// app.use(express.static("static"));
+// Enables EJS Layouts middleware
+app.use(ejsLayouts);
+// Sets up bodybody-parser for the parsing form data
+app.use(express.urlencoded({ extended: false }));
+// Adds some logging to each request
+app.use(require("morgan")("dev"));
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+
+
+// the app.listen function returns a server handle
+app.listen(PORT, () => {
+    console.log(`listening on PORT:${PORT}`);
+    rowdyResults.print();
+})
