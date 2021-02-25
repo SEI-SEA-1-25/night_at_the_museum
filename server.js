4e612e9e7eb1777f35a1aff714c4e7c64b47d536
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-24 21:34:36 -0800

    results page link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const ejsLayouts = require("express-ejs-layouts");
 const rowdy = require("rowdy-logger");
-const axios = require("axios")
+const axios = require("axios");
 // configure environmental variables
 require("dotenv").config();
 
@@ -26,13 +26,23 @@ app.use(require("morgan")("dev"));
 
 // ROUTES
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
-
+app.get("/results", async (req, res) => {
+  try {
+    const results = await axios.get(
+      `http://api.harvardartmuseums.org/object?apikey=${MUSEUM_API_KEY}&title=${req.query.search}&size=30`
+    );
+    // res.json({ records: results.data.records })
+    res.render("results", { records: results.data.records });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // the app.listen function returns a server handle
 app.listen(PORT, () => {
-    console.log(`listening on PORT:${PORT}`);
-    rowdyResults.print();
-})
+  console.log(`listening on PORT:${PORT}`);
+  rowdyResults.print();
+});
