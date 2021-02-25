8ecce7b575f370306a5021d071fd98c3b4f3428a
darrengp <ceo.darrenperry@gmail.com>
2021-02-25 07:42:52 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// required packages
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const rowdy = require("rowdy-logger");
+
+// configure express app
+const app = express();
+const PORT = 3000;
+// (╯°□°）╯︵ ┻━┻
+const rowdyResults = rowdy.begin(app);
+
+// Sets EJS as the view engine
+app.set("view engine", "ejs");
+// Specifies the location of the static assets folder
+app.use(express.static("static"));
+// Enables EJS Layouts middleware
+app.use(ejsLayouts);
+// Sets up body-parser for parsing form data
+app.use(express.urlencoded({ extended: false }));
+// Adds some logging to each request
+app.use(require("morgan")("dev"));
+
+// Routes
+app.get("/", function (req, res) {
+  res.send("Hello, backend!");
+});
+
+// The app.listen function returns a server handle
+app.listen(PORT, () => {
+  console.log(`listening on PORT:${PORT}`);
+  rowdyResults.print();
+});
